54d403ce3aafe50730ac97e3cc24a9b48a3a4267
Vidal Anguiano <vidal.anguiano@gmail.com>
2018-02-04 18:13:15 -0600

    Hi

--- server.py
+++ server.py
@@ -1,1 +1,26 @@
+import urllib
+import json
 import os
+from flask import flask
+from flask import request
+from flask import make_response
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+	req = request.get_json(silent = True, force = True)
+	print('Request:\n', json.dumps(req, indent=4))
+	# res = makeWebhookResult(req)
+	# res = json.dumps(res, indent=4)
+	# print(res)
+	# r = make_response(res)
+	# r.headers['Content-Type'] = 'application/json'
+	return req
+
+# def makeWebhokResult(req):
+# 	pri
+
+
+if __name__ == '__main__':
+	app.run()
