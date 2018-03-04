f6ebe265e694b52805a012567b193b4bbab168c6
Vidal Anguiano <vidal.anguiano@gmail.com>
2018-03-03 23:34:14 -0600

    dict

--- server.py
+++ server.py
@@ -190,7 +190,7 @@ def makeWebhookResult(req):
 #         return None
 
 
-def followupEvent(event_key, data = None):
+def followupEvent(event_key, data = {}):
     events = {'pothole': 'pothole_request','rodent': 'rodent_request',
               'street light': 'street_light_request',
               'completion_time': 'completion_time', 
@@ -202,7 +202,7 @@ def followupEvent(event_key, data = None):
     if data:
         return {"followupEvent": {"name": '{}'.format(event), "data": data}}
 
-    return {"followupEvent": {"name": '{}'.format(event)}}
+    # return {"followupEvent": {"name": '{}'.format(event)}}
 
 def capture_response(req):
     pass
