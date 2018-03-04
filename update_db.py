e58b212587a98d4cfe1e509e690702ad00a9bbcc
Loren Hinkson <loren.hinkson@gmail.com>
2018-03-03 20:20:40 -0600

    db creation, update, time only query

--- update_db.py
+++ update_db.py
@@ -1,5 +1,7 @@
 import psycopg2
 from dotenv import get_key, find_dotenv
+from sqlalchemy import create_engine
+import io
 
 USER = get_key(find_dotenv(), 'DB_USER')
 NAME = get_key(find_dotenv(), 'DB_NAME')
@@ -8,46 +10,207 @@ HOST = get_key(find_dotenv(), 'DB_HOST')
 PORT = get_key(find_dotenv(), 'DB_PORT')
 
 
+engine_string = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(USER, PW, HOST, PORT, NAME)    
+engine = create_engine(engine_string)
+# dialect+driver://username:password@host:port/database
+
 def create_db():
     connection_string = "dbname='{}' user='{}' host='{}' port='{}' password='{}'".format(NAME, USER, HOST, PORT, PW)
+
+    potholes_table = """
+    CREATE TABLE potholes (
+    creation_date date, 
+    status varchar(50), 
+    completion_date date, 
+    service_request_number varchar(11) PRIMARY KEY, 
+    type_of_service_request varchar(50), 
+    current_activity varchar(50), 
+    most_recent_action text,
+    number_of_potholes_filled_on_block numeric,
+    street_address text, 
+    zip varchar(10), 
+    x_coordinate double precision, 
+    y_coordinate double precision, 
+    ward varchar(2), 
+    police_district varchar(2), 
+    community_area varchar(2), 
+    ssa varchar(50), 
+    latitude double precision, 
+    longitude double precision, 
+    location varchar(50),
+    response_time interval);
+    """
+
+    rodents_table = """
+    CREATE TABLE rodents (
+    creation_date date, 
+    status varchar(50), 
+    completion_date date, 
+    service_request_number varchar(11) PRIMARY KEY, 
+    type_of_service_request varchar(50), 
+    number_of_premises_baited integer,
+    number_of_premises_with_garbage integer,
+    number_of_premises_with_rats integer,
+    current_activity varchar(50), 
+    most_recent_action text,
+    street_address text, 
+    zip varchar(10), 
+    x_coordinate double precision, 
+    y_coordinate double precision, 
+    ward varchar(2), 
+    police_district varchar(2), 
+    community_area varchar(2), 
+    ssa varchar(50), 
+    latitude double precision, 
+    longitude double precision, 
+    location varchar(50),
+    response_time interval);
+    """
+
+    streetlights_table = """
+    CREATE TABLE streetlights 
+    (
+    creation_date date, 
+    status varchar(50), 
+    completion_date date, 
+    service_request_number varchar(11) PRIMARY KEY, 
+    type_of_service_request varchar(50), 
+    street_address text, 
+    zip varchar(10), 
+    x_coordinate double precision, 
+    y_coordinate double precision, 
+    ward varchar(2), 
+    police_district varchar(2), 
+    community_area varchar(2), 
+    latitude double precision, 
+    longitude double precision, 
+    location varchar(50), 
+    response_time interval);
+    """
+
+    conn = psycopg2.connect(connection_string)
+    print("Connection made:", type(conn))
+
+    cur = conn.cursor()
+    print("created cursor")
+
     
-    try:
-        conn = psycopg2.connect(connection_string)
-        # conn = psycopg2.connect("dbname='chi311' user='civicchifecta' host='chi311.c1bdxz9lxp7b.us-east-2.rds.amazonaws.com' port='5432' password='trif3cta'")
-        print(type(conn))
+    cur.execute(potholes_table)
+    print("made potholes!")
 
-        cur = conn.cursor()
+    cur.execute(rodents_table)
+    print("made rodents!")
 
-        cur.execute("CREATE TABLE test3 (id serial PRIMARY KEY, num integer, data varchar);")
+    cur.execute(streetlights_table)
+    print("made streetlights!")
 
-        cur.execute("INSERT INTO test3 (num, data) VALUES (%s, %s)", (400, "Goodbye For Now"))
+    conn.commit()
+    cur.close()
 
-        cur.execute("SELECT * FROM test3;")
-        
-        test = cur.fetchone()
-        print(test)
-
-        conn.commit()
-        cur.close()
-        conn.close()
-
-    
-    except:
-        print("I am unable to connect to the database.")
+    conn.close()
 
 
-
-
-    # cur.executemany(query, vars_list)
-    # Execute a database operation (query or command) against all parameter 
-    # tuples or mappings found in the sequence vars_list.
-    # 
-    # mostly useful for commands that update the database: any result set 
-    # returned by the query is discarded.
+def create_initial_table(df, tablename):
+    conn=engine.raw_connection()
+    cur = conn.cursor()
+    output = io.StringIO()
+    df.to_csv(output, sep='\t', header=False, index=False)
+    output.seek(0)
+    contents = output.getvalue()
+    cur.copy_from(output, tablename, null="") # null values become ''   
+    conn.commit()
 
 
 
+def update_table(df, tablename):
+    # take de-duped update dataframe and create temporary table
+    df.to_sql('temp_table', engine, if_exists='replace', dtype = {'creation_date':sqlalchemy.types.DateTime, 'completion_date':sqlalchemy.types.DateTime, 'location':sqlalchemy.types.JSON})
 
+  
+    update_query = '''
+INSERT INTO potholes(creation_date, status, completion_date, service_request_number, 
+  type_of_service_request, current_activity, most_recent_action, 
+  number_of_potholes_filled_on_block, street_address, zip, x_coordinate, y_coordinate, 
+  ward, police_district, community_area, ssa, latitude, longitude, location, response_time)
+SELECT *
+FROM temp_table
+ON CONFLICT(svc_req_number) DO UPDATE
+    SET completion_date = COALESCE(excluded.completion_date, completion_date),
+        status = COALESCE(excluded.status, status),
+        current_activity = COALESCE(excluded.current_activity, current_activity),
+        most_recent_action = COALESCE(excluded.most_recent_action, most_recent_action);
+'''
+
+    with engine.begin() as conn:
+        print("beginning!")
+        conn.execute(update_query)
+        print("done!")
+
+
+
+
+def request_triggerd_query(tablename, input_latitude, input_longitude):
+    time_only ='''
+SELECT 
+CASE WHEN EXTRACT(DAY FROM AVG(response_time)) < 14
+THEN CEIL((EXTRACT(DAY FROM AVG(response_time))))
+END as days,
+CASE WHEN EXTRACT(DAY FROM AVG(response_time)) >= 14
+THEN CEIL(EXTRACT(DAY FROM AVG(response_time))/7)
+END as weeks,
+CASE WHEN EXTRACT(DAY FROM AVG(response_time)) > 30
+THEN justify_days(AVG(response_time))
+END as months
+FROM {} 
+JOIN (SELECT service_request_number, EXTRACT(WEEK FROM {}.creation_date) as week_nunm
+      FROM {} 
+      WHERE age(now(), creation_date) < '4 years' AND EXTRACT(WEEK FROM {}.creation_date) BETWEEN (EXTRACT(WEEK FROM now()) - 2) AND (EXTRACT(WEEK FROM now()) + 2) ) AS recent
+ON recent.service_request_number = {}.service_request_number;
+'''    
+
+
+
+    cur.execute(
+        psycopg2.sql.SQL(time_only)
+            .format(sql.Identifier(tablename)),
+        [input_date])
+
+    pass
+
+
+
+
+full_update_query = '''
+INSERT INTO potholes(creation_date, status, completion_date, service_request_number, type_of_service_request,
+  current_activity, most_recent_action, number_of_potholes_filled_on_block,
+  street_address, zip, x_coordinate, y_coordinate, ward, police_district,
+  community_area, ssa, latitude, longitude, location, response_time)
+SELECT tmp_potholes.*
+FROM tmp_potholes
+WHERE 
+ON CONFLICT(svc_req_number) DO UPDATE
+    SET completion_date = COALESCE(excluded.completion_date, completion_date),
+        status = COALESCE(excluded.status, status),
+        current_activity = COALESCE(excluded.current_activity, current_activity),
+        most_recent_action = COALESCE(excluded.most_recent_action, most_recent_action)
+    WHERE (excluded.completion_date IS DISTINCT FROM completion_date OR
+           excluded.status IS DISTINCT FROM  status OR
+           excluded.current_activity IS DISTINCT FROM  current_activity) OR
+           excluded.most_recent_action IS DISTINCT FROM most_recent_action)
+          );
+'''
+
+
+# CREATE OR REPLACE FUNCTION some_f(_tbl regclass, OUT result integer) AS
+# $func$
+# BEGIN
+# EXECUTE format('SELECT (EXISTS (SELECT 1 FROM %s WHERE id = 1))::int', _tbl)
+# INTO result;
+# END
+# $func$ LANGUAGE plpgsql;
+
+# # function call
+# SELECT some_f('myschema.mytable');
 
 
 
