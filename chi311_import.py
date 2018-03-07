8192d5f37745f0dfe3c4edc2d330a0fc0482d44a
Loren Hinkson <loren.hinkson@gmail.com>
2018-03-06 20:49:08 -0600

    Daily update done

--- chi311_import.py
+++ chi311_import.py
@@ -1,15 +1,29 @@
-import os
-from sodapy import Socrata
-from datetime import datetime, timedelta
-from dotenv import get_key, find_dotenv
-import requests
 import csv
-import pandas as pd
 import math
+import io
+import os
+import requests
+import psycopg2
+import pandas as pd
+from psycopg2 import sql
+from sodapy import Socrata
+from dotenv import get_key, find_dotenv
+from datetime import datetime, timedelta
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+
+USER = get_key(find_dotenv(), 'DB_USER')
+NAME = get_key(find_dotenv(), 'DB_NAME')
+PW = get_key(find_dotenv(), 'DB_PW')
+HOST = get_key(find_dotenv(), 'DB_HOST')
+PORT = get_key(find_dotenv(), 'DB_PORT')
+SSL = get_key(find_dotenv(), 'SSL')
+SSL_DIR = os.path.dirname(__file__)
+SSL_PATH = os.path.join(SSL_DIR, SSL)
+psycopg2_connection_string = "dbname='{}' user='{}' host='{}' port='{}' password='{}' sslmode='verify-full' sslrootcert='{}'".format(NAME, USER, HOST, PORT, PW, SSL_PATH)
 
 
 APP_TOKEN = get_key(find_dotenv(), 'SODAPY_APPTOKEN')
-
 DOMAIN = 'https://data.cityofchicago.org'
 
 
@@ -32,7 +46,12 @@ historicals = [{'service_name':'potholes',
                                'SSA': 'ssa', 
                                'LATITUDE': 'latitude', 
                                'LONGITUDE': 'longitude', 
-                               'LOCATION': 'location'},                                               
+                               'LOCATION': 'location'},
+                'order': ['creation_date', 'status', 'completion_date', 'service_request_number', 
+                          'type_of_service_request','current_activity', 'most_recent_action', 
+                          'number_of_potholes_filled_on_block','street_address', 'zip', 'x_coordinate', 
+                          'y_coordinate', 'ward','police_district', 'community_area', 'ssa', 
+                          'latitude', 'longitude', 'location'],
                 'url': 'https://data.cityofchicago.org/api/views/7as2-ds3y/rows.csv?accessType=DOWNLOAD&api_foundry=true',
                 'final_indicator': 'Final Outcome',
                 'endpoint':'787j-mys9'},
@@ -56,7 +75,13 @@ historicals = [{'service_name':'potholes',
                                'Community Area': 'community_area',
                                'Latitude': 'latitude',
                                'Longitude': 'longitude',
-                               'Location': 'location'}, 
+                               'Location': 'location'},
+                'order': ['creation_date', 'status', 'completion_date', 'service_request_number', 
+                          'type_of_service_request','number_of_premises_baited', 
+                          'number_of_premises_with_garbage', 'number_of_premises_with_rats', 
+                          'current_activity', 'most_recent_action', 'street_address', 
+                          'zip', 'x_coordinate', 'y_coordinate', 'ward','police_district', 
+                          'community_area', 'latitude', 'longitude', 'location'], 
                 'url': 'https://data.cityofchicago.org/api/views/97t6-zrhs/rows.csv?accessType=DOWNLOAD&api_foundry=true',
                 'final_indicator': 'Dispatch Crew',
                 'endpoint': 'dvua-vftq'},
@@ -76,6 +101,9 @@ historicals = [{'service_name':'potholes',
                                 'Latitude': 'latitude',
                                 'Longitude': 'longitude',
                                 'Location': 'location'},
+                'order': ['creation_date', 'status', 'completion_date', 'service_request_number', 
+                          'type_of_service_request', 'street_address', 'zip', 'x_coordinate', 'y_coordinate', 
+                          'ward', 'police_district', 'community_area', 'latitude', 'longitude', 'location'],
                 'url': 'https://data.cityofchicago.org/api/views/3aav-uy2v/rows.csv?accessType=DOWNLOAD&api_foundry=true',
                 'final_indicator': None,
                 'endpoint': 'h555-t6kz'}
@@ -96,7 +124,7 @@ def convert_dates(date_series):
     return date_series.map(dates)
 
 
-def build_initial_tables(historicals_list):
+def build_initial_tables(historicals):
     '''
     Create databases for each of the 311 services stored as keys in a given
     dictionary, each of which is a dictionary indicating a related CSV URL and 
@@ -104,7 +132,7 @@ def build_initial_tables(historicals_list):
     '''
     initial_records = []
 
-    for service_dict in historicals_list:
+    for service_dict in historicals:
         print("Starting: {}".format(service_dict['service_name']))
         try:
             r = requests.get(service_dict['url'])
@@ -131,7 +159,6 @@ def build_initial_tables(historicals_list):
                 
                 initial_records.append(historicals_df)
                 print("done, {:,} records".format(historicals_df.shape[0]))
-
 
         except:
             print("{}: Could not retrive CSV for {}".format(r.status_code, service_dict['service_name']))
@@ -178,7 +205,6 @@ def dedupe_df(df, service_dict):
 
     deduped_df = dupes.loc[keep_list]
     clean_df = df.append(deduped_df, ignore_index = True)
-    clean_df.set_index(keys = 'service_request_number', inplace = True)
 
     return clean_df
 
@@ -200,63 +226,159 @@ def check_updates(service_dict, days_back = 1):
     update_url = base_url + "&$limit={}&$where=:updated_at>'{}'".format(limit, period)
     
     check_result = requests.get(test_url)
-    print(test_url, "Code:", check_result.status_code)
-    python_check = check_result.json()
-    num_records = int(python_check[0]['count'])
-    print(num_records)
-    pulls = math.ceil(int(num_records) / limit)
-    print(pulls)
 
-    new_updates = requests.get(update_url)
+    if check_result.status_code != 200:
+        print("Unsuccessful result count request.")
+    else:
+        python_check = check_result.json()
+        num_records = int(python_check[0]['count'])
+        print(num_records)
+        pulls = math.ceil(int(num_records) / limit)
+        print(pulls)
+        new_updates = requests.get(update_url)
 
-    print(update_url, "Code:", new_updates.status_code)
-    
-    newly_updated = pd.DataFrame(new_updates.json())
-    print("pull #: 1")
-    print(newly_updated[:5])
-    
-    if pulls > 1:
-        
-        # perform HTTP GET request n - 1 more times and add to dataframe
-        update_list = [newly_updated]
-        for pull in range(pulls - 1):
-            print("pull #:", pull + 2)
-            offset_url = base_url + "&$limit={}&$offset={}&$where=:updated_at>'{}'".format(limit, offset_amt, period)
-            offset_amt += 2000
-           
-            next_pull = requests.get(offset_url)
-            print(offset_url, "Code:", next_pull.status_code)
-            next_pull_df = pd.DataFrame(next_pull.json())
-
-            update_list.append(next_pull_df)
-
+        if new_updates.status_code != 200:
+            print("Unsuccessful GET request.")
+        else:
+            newly_updated = pd.DataFrame(new_updates.json())
+            # print(newly_updated[:5])
             
-        newly_updated = pd.concat(update_list, ignore_index = True)
+            if pulls > 1:
+                
+                # perform HTTP GET request n - 1 more times and add to dataframe
+                update_list = [newly_updated]
+                for pull in range(pulls - 1):
+                    offset_url = base_url + "&$limit={}&$offset={}&$where=:updated_at>'{}'".format(limit, offset_amt, period)
+                    offset_amt += 2000
+                   
+                    next_pull = requests.get(offset_url)
+                    next_pull_df = pd.DataFrame(next_pull.json())
+
+                    update_list.append(next_pull_df)
+
+                newly_updated = pd.concat(update_list, ignore_index = True)
+            
+            # ADD TEST FOR UNEXPECTED NAMES HERE
+            print(newly_updated.columns)
+            print()
+            
+            # newly_updated.rename(columns = service_dict['clean_cols'], inplace=True)
+            ordered_updates = newly_updated.reindex(columns = service_dict['order'])
+            print(ordered_updates.columns)
+
+            # print(newly_updated[:5])
+            ordered_updates['creation_date'] = convert_dates(ordered_updates['creation_date'])
+            ordered_updates['completion_date'] = convert_dates(ordered_updates['completion_date'])
+            ordered_updates['response_time'] = ordered_updates['completion_date'] - ordered_updates['creation_date']
+
+            return ordered_updates
+
+
+
+def update_table(df, tablename):
+    # take de-duped update dataframe and create temporary table
+    update_temp_table_query = '''
+    CREATE TEMP TABLE tmp_table
+    AS
+    SELECT *
+    FROM {tbl}
+    WITH NO DATA;
+    '''
     
-    newly_updated.rename(columns = service_dict['clean_cols'], inplace=True)
-    print(newly_updated[:5])
-    newly_updated['creation_date'] = convert_dates(newly_updated['creation_date'])
-    newly_updated['completion_date'] = convert_dates(newly_updated['completion_date'])
 
-    return newly_updated
+    if tablename == 'streetlights':
+        load_updates_query = '''
+INSERT INTO {tbl} (creation_date, status, completion_date, service_request_number, 
+               type_of_service_request, street_address, zip, x_coordinate, y_coordinate, 
+               ward, police_district, community_area, latitude, longitude, location, response_time)
+SELECT *
+FROM tmp_table
+ON CONFLICT(service_request_number) DO UPDATE
+    SET completion_date = COALESCE(excluded.completion_date, {tbl}.completion_date),
+        status = COALESCE(excluded.status, {tbl}.status),
+        response_time = COALESCE(excluded.response_time, {tbl}.response_time)
+        WHERE (excluded.completion_date IS DISTINCT FROM {tbl}.completion_date) OR
+           (excluded.status IS DISTINCT FROM  {tbl}.status) OR
+           (excluded.response_time IS DISTINCT FROM {tbl}.response_time);'''
+    
+
+    elif tablename == 'potholes': 
+        load_updates_query = '''
+INSERT INTO {tbl} (creation_date, status, completion_date, service_request_number,
+               type_of_service_request, current_activity, most_recent_action,
+               number_of_potholes_filled_on_block, street_address, zip, x_coordinate, y_coordinate,
+               ward, police_district, community_area, ssa, latitude, longitude, location, response_time)
+SELECT *
+FROM tmp_table
+ON CONFLICT(service_request_number) DO UPDATE
+    SET completion_date = COALESCE(excluded.completion_date, {tbl}.completion_date),
+        status = COALESCE(excluded.status, {tbl}.status),
+        current_activity = COALESCE(excluded.current_activity, {tbl}.current_activity),
+        most_recent_action = COALESCE(excluded.most_recent_action, {tbl}.most_recent_action),
+        response_time = COALESCE(excluded.response_time, {tbl}.response_time)
+        WHERE (excluded.completion_date IS DISTINCT FROM {tbl}.completion_date) OR
+           (excluded.status IS DISTINCT FROM  {tbl}.status) OR
+           (excluded.current_activity IS DISTINCT FROM  {tbl}.current_activity) OR
+           (excluded.most_recent_action IS DISTINCT FROM {tbl}.most_recent_action) OR 
+           (excluded.response_time IS DISTINCT FROM {tbl}.response_time);'''
 
 
 
-def daily_db_update(historicals_list): 
+    elif tablename == 'rodents': 
+        load_updates_query = '''
+INSERT INTO {tbl} (creation_date, status, completion_date, service_request_number, 
+               type_of_service_request, number_of_premises_baited, number_of_premises_with_garbage,
+               number_of_premises_with_rats, current_activity, most_recent_action, street_address, 
+               zip, x_coordinate, y_coordinate, ward, police_district, community_area, latitude, 
+               longitude, location, response_time)
+SELECT *
+FROM tmp_table
+ON CONFLICT(service_request_number) DO UPDATE
+    SET completion_date = COALESCE(excluded.completion_date, {tbl}.completion_date),
+        status = COALESCE(excluded.status, {tbl}.status),
+        current_activity = COALESCE(excluded.current_activity, {tbl}.current_activity),
+        most_recent_action = COALESCE(excluded.most_recent_action, {tbl}.most_recent_action),
+        response_time = COALESCE(excluded.response_time, {tbl}.response_time)
+        WHERE (excluded.completion_date IS DISTINCT FROM {tbl}.completion_date) OR
+           (excluded.status IS DISTINCT FROM  {tbl}.status) OR
+           (excluded.current_activity IS DISTINCT FROM  {tbl}.current_activity) OR
+           (excluded.most_recent_action IS DISTINCT FROM {tbl}.most_recent_action) OR 
+           (excluded.response_time IS DISTINCT FROM {tbl}.response_time);'''
+
+    
+    temp_table_q = sql.SQL(update_temp_table_query).format(tbl=sql.Identifier(tablename))
+    load_table_q = sql.SQL(load_updates_query).format(tbl=sql.Identifier(tablename))
+
+    with psycopg2.connect(psycopg2_connection_string) as conn2:
+        with conn2.cursor() as cur:
+            print("beginning upload...")
+            cur.execute(temp_table_q)
+            print("made temp table!")
+
+            output = io.StringIO()
+            df.to_csv(output, sep='\t', header=False, index=False)
+            output.seek(0)
+        
+            cur.copy_from(output, "tmp_table", null="")   
+            print(load_table_q)
+
+            cur.execute(load_table_q)
+            conn2.commit()
+
+
+
+def daily_db_update(historicals_list, days_back = 1): 
+    # add error flag, maybe trigger email?
     all_updates = []
     for service_dict in historicals_list:
-        updated = check_updates(service_dict)
+        print("starting {}...".format(service_dict['service_name']))
+        updated = check_updates(service_dict, days_back)
         clean_updates = dedupe_df(updated, service_dict)
-        all_updates.append(updated)
-    return all_updates
+        update_table(clean_updates, service_dict['service_name'])
+    return "completed without error"
     
 
-
-def go():
-    potholes, rodents, streetlights = build_initial_tables(historicals)
-    clean_ph = dedupe_df(potholes, historicals)
-    clean_rd = dedupe_df(rodents, historicals)
-    clean_sl = dedupe_df(streetlights, historicals)
-    
-    return (potholes, rodents, streetlights)
  
+
+if __name__ == '__main__':
+    daily_db_update(historicals)
