e58a0c84eaab4fbe91acd7db3d9a2025a1623239
Loren Hinkson <loren.hinkson@gmail.com>
2018-03-06 20:50:11 -0600

    Average response time done

--- update_db.py
+++ update_db.py
@@ -1,6 +1,7 @@
 import psycopg2
 from dotenv import get_key, find_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from psycopg2 import sql
 import io
 import os
 
@@ -19,10 +20,10 @@ SSL_PATH = os.path.join(SSL_DIR, SSL)
 engine_string = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(USER, PW, HOST, PORT, NAME)    
 ssl_args = {"sslmode": "require", "sslrootcert": SSL_PATH}
 engine = create_engine(engine_string, connect_args=ssl_args)
-# postgresql://civicchifecta:trif3cta@chi311.c1bdxz9lxp7b.us-east-2.rds.amazonaws.com:5432/chi311??sslrootcert=rds-combined-ca-bundle.pem&sslmode=require
+psycopg2_connection_string = "dbname='{}' user='{}' host='{}' port='{}' password='{}' sslmode='verify-full' sslrootcert='{}'".format(NAME, USER, HOST, PORT, PW, SSL_PATH)
 
-def create_db():
-    connection_string = "dbname='{}' user='{}' host='{}' port='{}' password='{}'".format(NAME, USER, HOST, PORT, PW)
+
+def create_table(tablename):
 
     potholes_table = """
     CREATE TABLE potholes (
@@ -55,9 +56,9 @@ def create_db():
     completion_date date, 
     service_request_number varchar(11) PRIMARY KEY, 
     type_of_service_request varchar(50), 
-    number_of_premises_baited integer,
+    number_of_premises_baited numeric,
     number_of_premises_with_garbage integer,
-    number_of_premises_with_rats integer,
+    number_of_premises_with_rats numeric,
     current_activity varchar(50), 
     most_recent_action text,
     street_address text, 
@@ -67,7 +68,6 @@ def create_db():
     ward varchar(2), 
     police_district varchar(2), 
     community_area varchar(2), 
-    ssa varchar(50), 
     latitude double precision, 
     longitude double precision, 
     location varchar(50),
@@ -95,10 +95,36 @@ def create_db():
     response_time interval);
     """
 
-    conn = psycopg2.connect(connection_string)
-    print("Connection made:", type(conn))
+    def create_table(tablename):
 
-    cur = conn.cursor()
+    potholes_table = """
+    CREATE TABLE potholes (
+    creation_date date, 
+    status varchar(50), 
+    completion_date date, 
+    service_request_number varchar(11) PRIMARY KEY, 
+    type_of_service_request varchar(50), 
+    current_activity varchar(50), 
+    most_recent_action text,
+    number_of_potholes_filled_on_block numeric,
+    street_address text, 
+    zip varchar(10), 
+    x_coordinate double precision, 
+    y_coordinate double precision, 
+    ward varchar(2), 
+    police_district varchar(2), 
+    community_area varchar(2), 
+    ssa varchar(50), 
+    latitude double precision, 
+    longitude double precision, 
+    location varchar(50),
+    response_time interval);
+    """
+    
+    conn2 = psycopg2.connect(psycopg2_connection_string)
+    print("Connection made:", type(conn2))
+
+    cur = conn2.cursor()
     print("created cursor")
 
     
@@ -111,108 +137,161 @@ def create_db():
     cur.execute(streetlights_table)
     print("made streetlights!")
 
-    conn.commit()
+    conn2.commit()
     cur.close()
+    conn2.close()
 
-    conn.close()
 
 
-def create_initial_table(df, tablename):
+def fill_initial_table(df, tablename):
     conn=engine.raw_connection()
     cur = conn.cursor()
     output = io.StringIO()
     df.to_csv(output, sep='\t', header=False, index=False)
     output.seek(0)
-    contents = output.getvalue()
     cur.copy_from(output, tablename, null="") # null values become ''   
     conn.commit()
+    conn.close()
 
 
 
-def update_table(df, tablename):
-    # take de-duped update dataframe and create temporary table
-    df.to_sql('temp_table', engine, if_exists='replace', dtype = {'creation_date':sqlalchemy.types.DateTime, 'completion_date':sqlalchemy.types.DateTime, 'location':sqlalchemy.types.JSON})
-
-  
-    update_query = '''
-INSERT INTO potholes(creation_date, status, completion_date, service_request_number, 
-  type_of_service_request, current_activity, most_recent_action, 
-  number_of_potholes_filled_on_block, street_address, zip, x_coordinate, y_coordinate, 
-  ward, police_district, community_area, ssa, latitude, longitude, location, response_time)
-SELECT *
-FROM temp_table
-ON CONFLICT(svc_req_number) DO UPDATE
-    SET completion_date = COALESCE(excluded.completion_date, completion_date),
-        status = COALESCE(excluded.status, status),
-        current_activity = COALESCE(excluded.current_activity, current_activity),
-        most_recent_action = COALESCE(excluded.most_recent_action, most_recent_action);
-'''
-
-    with engine.begin() as conn:
-        print("beginning!")
-        conn.execute(update_query)
-        print("done!")
-
-
+           
 
 
 def request_triggerd_query(tablename, input_latitude, input_longitude):
     time_only ='''
 SELECT 
-CASE WHEN EXTRACT(DAY FROM AVG(response_time)) < 14
-THEN CEIL((EXTRACT(DAY FROM AVG(response_time))))
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) < 14
+THEN CEIL((EXTRACT(DAY FROM AVG("response_time"))))
 END as days,
-CASE WHEN EXTRACT(DAY FROM AVG(response_time)) >= 14
-THEN CEIL(EXTRACT(DAY FROM AVG(response_time))/7)
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) >= 14
+THEN CEIL(EXTRACT(DAY FROM AVG("response_time"))/7)
 END as weeks,
-CASE WHEN EXTRACT(DAY FROM AVG(response_time)) > 30
-THEN justify_days(AVG(response_time))
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) > 60
+THEN justify_days(AVG("response_time"))
 END as months
-FROM {} 
-JOIN (SELECT service_request_number, EXTRACT(WEEK FROM {}.creation_date) as week_nunm
-      FROM {} 
-      WHERE age(now(), creation_date) < '4 years' AND EXTRACT(WEEK FROM {}.creation_date) BETWEEN (EXTRACT(WEEK FROM now()) - 2) AND (EXTRACT(WEEK FROM now()) + 2) ) AS recent
-ON recent.service_request_number = {}.service_request_number;
-'''    
-
-
-
-    cur.execute(
-        psycopg2.sql.SQL(time_only)
-            .format(sql.Identifier(tablename)),
-        [input_date])
-
-    pass
-
-
-
-
-full_update_query = '''
-INSERT INTO potholes(creation_date, status, completion_date, service_request_number, type_of_service_request,
-  current_activity, most_recent_action, number_of_potholes_filled_on_block,
-  street_address, zip, x_coordinate, y_coordinate, ward, police_district,
-  community_area, ssa, latitude, longitude, location, response_time)
-SELECT tmp_potholes.*
-FROM tmp_potholes
-WHERE 
-ON CONFLICT(svc_req_number) DO UPDATE
-    SET completion_date = COALESCE(excluded.completion_date, completion_date),
-        status = COALESCE(excluded.status, status),
-        current_activity = COALESCE(excluded.current_activity, current_activity),
-        most_recent_action = COALESCE(excluded.most_recent_action, most_recent_action)
-    WHERE (excluded.completion_date IS DISTINCT FROM completion_date OR
-           excluded.status IS DISTINCT FROM  status OR
-           excluded.current_activity IS DISTINCT FROM  current_activity) OR
-           excluded.most_recent_action IS DISTINCT FROM most_recent_action)
-          );
+FROM {} a
+JOIN (SELECT service_request_number, EXTRACT(WEEK FROM a.creation_date) as week_nunm
+      FROM {} a 
+      WHERE age(now(), creation_date) < '4 years' 
+      AND status IN ('Completed', 'Completed - Dup')
+      AND EXTRACT(WEEK FROM a.creation_date) BETWEEN (EXTRACT(WEEK FROM now()) - 2) AND (EXTRACT(WEEK FROM now()) + 2) ) AS recent
+ON recent.service_request_number = a.service_request_number;
 '''
 
+    
+    loc_only_neighborhood = '''
+SELECT 
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) < 14
+THEN CEIL((EXTRACT(DAY FROM AVG("response_time"))))
+END as days,
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) >= 14
+THEN CEIL(EXTRACT(DAY FROM AVG("response_time"))/7)
+END as weeks,
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) > 60
+THEN justify_days(AVG("response_time"))
+END as months
+FROM {tbl} a
+INNER JOIN neighborhoods b ON ST_Within(ST_SetSRID(ST_MakePoint(a.longitude, a.latitude),4326), b.geom)
+WHERE b.gid IN (SELECT b.gid FROM neighborhoods b  
+WHERE ST_Contains(b.geom, ST_SetSRID(ST_MakePoint(%s, %s),4326)))
+AND status IN ('Completed', 'Completed - Dup')
+AND age(now(), creation_date) < '2 years' 
+'''
 
-# location query
-# user_input_lat = 41.885001
-# user_input_lon = -87.645939
-# pass_in = (user_input_lon, user_input_lat)
-# '$where=within_circle(location, 41.885001, -87.645939, 41.867011, -87.618516)'
+    loc_only_radius = '''
+SELECT 
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) < 14
+THEN CEIL((EXTRACT(DAY FROM AVG("response_time"))))
+END as days,
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) >= 14
+THEN CEIL(EXTRACT(DAY FROM AVG("response_time"))/7)
+END as weeks,
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) > 60
+THEN justify_days(AVG("response_time"))
+END as months
+FROM streetlights 
+JOIN (SELECT service_request_number, EXTRACT(WEEK FROM streetlights.creation_date) as week_nunm
+      FROM streetlights
+      WHERE age(now(), creation_date) < '2 years' 
+      AND status IN ('Completed', 'Completed - Dup')
+      AND earth_box( ll_to_earth(%s , %s), 1000) @> ll_to_earth(streetlights.latitude, streetlights.longitude)) AS recent
+ON recent.service_request_number = streetlights.service_request_number;
+    '''
+
+    time_loc_radius = '''
+SELECT 
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) < 14
+THEN CEIL((EXTRACT(DAY FROM AVG("response_time"))))
+END as days,
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) >= 14
+THEN CEIL(EXTRACT(DAY FROM AVG("response_time"))/7)
+END as weeks,
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) > 60
+THEN justify_days(AVG("response_time"))
+END as months
+FROM {} a 
+JOIN (SELECT service_request_number, EXTRACT(WEEK FROM streetlights.creation_date) as week_nunm
+      FROM streetlights
+      WHERE age(now(), creation_date) < '4 years' 
+      AND status IN ('Completed', 'Completed - Dup')
+      AND EXTRACT(WEEK FROM a.creation_date) BETWEEN (EXTRACT(WEEK FROM now()) - 2) 
+      AND (EXTRACT(WEEK FROM now()) + 2) 
+      AND earth_box( ll_to_earth([lat], [lon]), 1000) @> ll_to_earth(a.latitude, streetlights.longitude)) AS recent
+ON recent.service_request_number = streetlights.service_request_number;
+    '''
+
+
+    time_loc_neighborhood = '''
+SELECT 
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) < 14
+THEN CEIL((EXTRACT(DAY FROM AVG("response_time"))))
+END as days,
+CASE WHEN EXTRACT(DAY FROM AVG(response_time)) >= 14
+THEN CEIL(EXTRACT(DAY FROM AVG("response_time"))/7)
+END as weeks,
+CASE WHEN EXTRACT(DAY FROM AVG("response_time")) > 60
+THEN justify_days(AVG("response_time"))
+END as months
+FROM {tbl} a
+INNER JOIN neighborhoods b ON ST_Within(ST_SetSRID(ST_MakePoint(a.longitude, a.latitude),4326), b.geom)
+WHERE b.gid IN (SELECT b.gid FROM neighborhoods b  
+WHERE ST_Contains(b.geom, ST_SetSRID(ST_MakePoint(%s, %s),4326)))
+AND age(now(), creation_date) < '4 years' 
+AND status IN ('Completed', 'Completed - Dup')
+AND EXTRACT(WEEK FROM a.creation_date) BETWEEN (EXTRACT(WEEK FROM now()) - 2) AND (EXTRACT(WEEK FROM now()) + 2);
+'''
+    
+    both_q = sql.SQL(time_loc_neighborhood).format(tbl=sql.Identifier(tablename))
+    loc_q = sql.SQL(time_loc_neighborhood).format(tbl=sql.Identifier(tablename))
+    time_q = sql.SQL(time_loc_neighborhood).format(tbl=sql.Identifier(tablename))
+   
+
+    conn2 = psycopg2.connect(psycopg2_connection_string)
+    cur = conn2.cursor()
+    cur.execute(both_q, [input_longitude, input_latitude])
+    res = cur.fetchone()
+    print(res)
+
+    if all(v is None for v in res):
+        print("checking time only")
+        cur.execute(loc_q, [input_longitude, input_latitude])
+        res = cur.fetchone()
+        print(res)
+
+        if all(v is None for v in res):
+            print("checking time ony")
+            cur.execute(time_q, [input_longitude, input_latitude])
+            res = cur.fetchone()
+            print(res)
+
+            if all(v is None for v in res):
+                return "I'm sorry, we can't find the average resolution time for {} requests this time of year in your area."
+
+    cur.close()
+    conn2.close()
+
+    return res
 
 
 
