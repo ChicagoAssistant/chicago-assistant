7e1bccecb7b12e37f827c16c06e4753487ec66e1
Loren Hinkson <loren.hinkson@gmail.com>
2018-03-08 21:30:18 -0600

    added logging

--- clock.py
+++ clock.py
@@ -3,17 +3,27 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from dotenv import get_key, find_dotenv
+from datetime import datetime
 import os
+import logging
 
-USER = os.environ['DB_USER']
-NAME = os.environ['DB_NAME']
-PW = os.environ['DB_PW']
-HOST = os.environ['DB_HOST']
-PORT = os.environ['DB_PORT']
+# USER = os.environ['DB_USER']
+# NAME = os.environ['DB_NAME']
+# PW = os.environ['DB_PW']
+# HOST = os.environ['DB_HOST']
+# PORT = os.environ['DB_PORT']
+# SSL_DIR = os.path.dirname(__file__)
+# SSL = os.environ['SSL']
+# SSL_PATH = os.path.join(SSL_DIR, SSL)
+
+USER = get_key(find_dotenv(), 'DB_USER')
+NAME = get_key(find_dotenv(), 'DB_NAME')
+PW = get_key(find_dotenv(), 'DB_PW')
+HOST = get_key(find_dotenv(), 'DB_HOST')
+PORT = get_key(find_dotenv(), 'DB_PORT')
+SSL = get_key(find_dotenv(), 'SSL')
 SSL_DIR = os.path.dirname(__file__)
-SSL = os.environ['SSL']
 SSL_PATH = os.path.join(SSL_DIR, SSL)
-
 
 engine_string = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(USER, PW, HOST, PORT, NAME) 
 ssl_args = {"sslmode": "require", "sslrootcert": SSL_PATH}
@@ -22,6 +32,8 @@ jobstores = {'default': SQLAlchemyJobStore(url=engine_string)}
 
 job_defaults = {'coalesce': True, 'misfire_grace_time': 20}
 
+
+logging.basicConfig(filename='dailyUpdateLog.txt', level=logging.DEBUG)
 
 
 if __name__ == '__main__':
@@ -34,10 +46,10 @@ if __name__ == '__main__':
                 updated = check_updates(service_dict, days_back)
                 clean_updates = dedupe_df(updated, service_dict)
                 update_table(clean_updates, service_dict['service_name'])
-                print("completed without error")
+                logging.info("completed without error at {}.".format(datetime.now()))
         except Exception as e:
-            print("Update failed: {}".format(e))
+            logging.info("Encountered error: {} at {}.".format(e, datetime.now()))
+        
 
     job = scheduler.add_job(daily_db_update, 'cron', day_of_week='0-6', hour=10, minute=12, args=[historicals])
-    
     scheduler.start()
