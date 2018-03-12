3ef9911106c5d23bb191e62c751e43246b64cd24
Loren Hinkson <loren.hinkson@gmail.com>
2018-03-12 17:54:19 -0500

    got it

---
+++ queries.py
@@ -0,0 +1,69 @@
+TIME_ONLY ='''
+    SELECT
+    CASE WHEN EXTRACT(DAY FROM AVG("response_time")) > 60
+    THEN justify_days(AVG("response_time"))
+    END as months,
+    CASE WHEN EXTRACT(DAY FROM AVG("response_time")) >= 14
+    THEN CEIL(EXTRACT(DAY FROM AVG("response_time"))/7)
+    END as weeks,
+    CASE WHEN EXTRACT(DAY FROM AVG("response_time")) < 14
+    THEN CEIL((EXTRACT(DAY FROM AVG("response_time"))))
+    END as days
+    FROM {} a
+    INNER JOIN (SELECT service_request_number, EXTRACT(WEEK FROM a.creation_date) as week_nunm
+      FROM {} a 
+      WHERE age(now(), creation_date) < '4 years' 
+      AND status IN ('Completed', 'Completed - Dup')
+      AND EXTRACT(WEEK FROM a.creation_date) BETWEEN (EXTRACT(WEEK FROM now()) - 2) AND (EXTRACT(WEEK FROM now()) + 2) ) AS recent
+    ON recent.service_request_number = a.service_request_number;'''
+
+    
+LOC_ONLY = '''
+    SELECT
+    CASE WHEN EXTRACT(DAY FROM AVG("response_time")) > 60
+    THEN justify_days(AVG("response_time"))
+    END as months,
+    CASE WHEN EXTRACT(DAY FROM AVG("response_time")) >= 14
+    THEN CEIL(EXTRACT(DAY FROM AVG("response_time"))/7)
+    END as weeks,
+    SELECT b.pri_neigh as neighborhood,
+    CASE WHEN EXTRACT(DAY FROM AVG("response_time")) < 14
+    THEN CEIL((EXTRACT(DAY FROM AVG("response_time"))))
+    END as days,
+    FROM {tbl} a
+    INNER JOIN neighborhoods b ON ST_Within(ST_SetSRID(ST_MakePoint(a.longitude, a.latitude),4326), b.geom)
+    WHERE b.gid IN (SELECT b.gid FROM neighborhoods b  
+    WHERE ST_Contains(b.geom, ST_SetSRID(ST_MakePoint(%s, %s),4326)))
+    AND status IN ('Completed', 'Completed - Dup')
+    AND age(now(), creation_date) < '2 years' 
+    GROUP BY b.pri_neigh;'''
+
+
+TIME_LOC = '''
+    SELECT b.pri_neigh as neighborhood,
+    CASE WHEN EXTRACT(DAY FROM AVG("response_time")) > 60
+    THEN justify_days(AVG("response_time"))
+    END as months,
+    CASE WHEN EXTRACT(DAY FROM AVG(response_time)) >= 14
+    THEN CEIL(EXTRACT(DAY FROM AVG("response_time"))/7)
+    END as weeks,
+    CASE WHEN EXTRACT(DAY FROM AVG("response_time")) < 14
+    THEN CEIL((EXTRACT(DAY FROM AVG("response_time"))))
+    END as days
+    FROM {tbl} a
+    INNER JOIN neighborhoods b ON ST_Within(ST_SetSRID(ST_MakePoint(a.longitude, a.latitude),4326), b.geom)
+    WHERE b.gid IN (SELECT b.gid FROM neighborhoods b  
+    WHERE ST_Contains(b.geom, ST_SetSRID(ST_MakePoint(%s, %s),4326)))
+    AND age(now(), creation_date) < '4 years' 
+    AND status IN ('Completed', 'Completed - Dup')
+    AND EXTRACT(WEEK FROM a.creation_date) BETWEEN (EXTRACT(WEEK FROM now()) - 2) AND (EXTRACT(WEEK FROM now()) + 2)
+    GROUP BY b.pri_neigh;'''
+
+RECORD_TRANSACTION ='''
+    INSERT INTO dialogflow_transactions (session_Id, request_time, 
+    service_type, description, request_details, address_string, lat, lng, email, 
+    first_name, last_name, phone, open_311_status, token)
+    VALUES (%s,  %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    '''
+    
+
