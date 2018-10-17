969b18f37ce8d7d0cb48ca244e35f8aa89a4acb6
madisoncarr <madison.carr@utexas.edu>
2018-10-16 19:57:20 -0500

    run script

---
+++ run.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+docker run --rm --name chicago-assistant -it -v /Users/cynicalbrunette/Documents/projects/chicago-assistant:/chicago-assistant chicagoassistant/chicago-assistant bash
+
+
